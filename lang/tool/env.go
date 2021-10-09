e9f2f0e1bf26899fe2d8f96afe2627bff7ada55d
Preston Koprivica <pkopriv2@gmail.com>
2021-10-09 16:05:08 -0500

    Removing default config file

--- lang/tool/env.go
+++ lang/tool/env.go
@@ -3,20 +3,12 @@ package tool
 import (
 	"os"
 
+	"github.com/pkg/errors"
 	"github.com/pkopriv2/golang-sdk/lang/config"
 	"github.com/pkopriv2/golang-sdk/lang/context"
 	"github.com/pkopriv2/golang-sdk/lang/path"
 	"github.com/pkopriv2/golang-sdk/lang/term"
-	"github.com/pkg/errors"
 )
-
-var DefaultConfigFile = "~/.stash/config.yaml"
-
-func init() {
-	if path := os.Getenv("STASH_CONFIG_FILE"); path != "" {
-		DefaultConfigFile = path
-	}
-}
 
 type Environment struct {
 	Context  context.Context
@@ -24,24 +16,24 @@ type Environment struct {
 	Terminal term.Terminal
 }
 
-func NewDefaultEnvironment() (ret Environment, err error) {
-	path, err := path.Expand(DefaultConfigFile)
+func NewEnvironment(configFile string) (ret Environment, err error) {
+	path, err := path.Expand(configFile)
 	if err != nil {
-		err = errors.Wrapf(err, "Unable to expand path [%v]", DefaultConfigFile)
+		err = errors.Wrapf(err, "Unable to expand path [%v]", configFile)
 		return
 	}
 
 	conf := config.NewConfig()
 	if _, err := os.Stat(path); err == nil {
-		conf, err = config.ParseFromFile(DefaultConfigFile)
+		conf, err = config.ParseFromFile(configFile)
 		if err != nil {
-			err = errors.Wrapf(err, "Unable to parse config file [%v]", DefaultConfigFile)
+			err = errors.Wrapf(err, "Unable to parse config file [%v]", configFile)
 			return ret, err
 		}
 	}
 
 	var lvl string
-	if err = conf.GetOrDefault("stash.log.level", config.String, &lvl, context.Off.String()); err != nil {
+	if err = conf.GetOrDefault("log.level", config.String, &lvl, context.Off.String()); err != nil {
 		return
 	}
 
