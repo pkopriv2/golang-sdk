13caf133af659ab7da27a28ffda1425061940d9f
Preston Koprivica <pkopriv2@gmail.com>
2021-10-30 09:34:56 -0500

    Removing bourne dependency

--- lang/stash/stash.go
+++ lang/stash/stash.go
@@ -14,16 +14,6 @@ func OpenRandom() (*bolt.DB, error) {
 	return Open(path.Join(afero.GetTempDir(afero.NewOsFs(), path.Join("stash", uuid.NewV1().String())), "stash.db"))
 }
 
-// Opens a transient stash instance that will be deleted on ctx#close().
-func OpenTransient() (*bolt.DB, error) {
-	stash, err := OpenRandom()
-	if err != nil {
-		return nil, err
-	}
-
-	return stash, nil
-}
-
 func Open(path string) (*bolt.DB, error) {
 	return bolt.Open(path, 0666, &bolt.Options{Timeout: 10 * time.Second})
 }
